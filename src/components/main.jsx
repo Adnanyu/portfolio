989bb7451717b22bd52af19e290937c5c77a66eb
adnan <adnanyu>
2023-02-04 16:47:26 +0300

    added the media

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -4,7 +4,7 @@ const Main = () => {
             <div className="main-text">
                 <h1 className="main-header">hello there!!!, I'am aden</h1>
                 <p> I am afrontend developer.</p>
-                <a href="zjh" className='profile-links'>resume</a> 
+                <a href="" className='profile-links'>resume</a> 
                 <a href="https://github.com/Adnanyu" target='_blank' rel="noreferrer" className='profile-links'>github</a>
             </div>
         </main>
