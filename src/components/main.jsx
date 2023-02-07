69d38e2352c3b11ed588f6ae3097a94dcb5eaacf
adnan <adnanyu>
2023-02-07 20:02:12 +0300

    fixed the pdf link

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,11 @@
+import Pdf from '../assets/adenyusuf.pdf'
 const Main = () => {
     return (
         <main className='main-section'>
             <div className="main-text">
                 <h1 className="main-header">hello there!!!, I'am aden</h1>
                 <p> I am afrontend developer.</p>
-                <a href="../assets/adenyusuf.pdf" className='profile-links' download>resume</a> 
+                <a href={Pdf} className='profile-links' target='_blank' rel='noopener noreferrer'>resume</a> 
                 <a href="https://github.com/Adnanyu" target='_blank' rel="noreferrer" className='profile-links'>github</a>
             </div>
         </main>
