661ebba96862d1b341664acf46afab7377935216
adnan <adnanyu>
2023-04-06 02:00:51 +0300

    changed the resume

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,12 +1,12 @@
-import Pdf from '../assets/aden2.pdf'
+import Pdf from '../assets/Resume.pdf'
 const Main = () => {
     return (
         <main className='main-section'>
             <div className="main-text">
                 <h1 className="main-header">hello there! I'am aden</h1>
                 <p> I am a frontend developer.</p>
-                <a href={Pdf} className='profile-links' target='_blank' rel='noopener noreferrer'>resume</a> 
-                <a href="https://github.com/Adnanyu" target='_blank' rel="noreferrer" className='profile-links'>github</a>
+                <a href={Pdf} className='profile-links' target='_blank' rel='noopener noreferrer'>Resume</a> 
+                <a href="https://github.com/Adnanyu" target='_blank' rel="noreferrer" className='profile-links'>Github</a>
             </div>
         </main>
     )
