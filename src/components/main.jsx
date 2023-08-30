54db59ad2ce17ca6d6f3356898611779c2b873cb
adnan <adnanyu>
2023-08-30 19:53:19 +0300

    added new resume

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -4,7 +4,7 @@ const Main = () => {
         <main className='main-section'>
             <div className="main-text">
                 <h1 className="main-header">hello there! I'am aden</h1>
-                <p> I am a frontend developer.</p>
+                <p> I am a full-stack developer.</p>
                 <a href={Pdf} className='profile-links' target='_blank' rel='noopener noreferrer'>Resume</a> 
                 <a href="https://github.com/Adnanyu" target='_blank' rel="noreferrer" className='profile-links'>Github</a>
             </div>
