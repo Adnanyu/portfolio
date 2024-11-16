4a9fb86407a1012ae58345c58606b65c6cf6bf95
adnan <adnanyu>
2024-11-16 20:21:45 +0300

    changed my name and updated my resume :)

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,7 +4,7 @@ const About = () => {
             <div className="about-container">
                 <h2 className="about-title">About me</h2>
                 <p className="about">
-                I am a computer engineering student based in Istanbul, with a passion for front-end development. With a strong drive for self-education and a natural curiosity for technology. My portfolio showcases my ability to turn ideas into reality through the use of HTML, CSS, JavaScript, NodeJs, ExpressJs, MongoDB React and other web deveolping technologies. I am constantly pushing my limits and striving to learn new skills.
+                I am a computer engineering student based in Istanbul, with a passion for full-stack development. With a strong drive for self-education and a natural curiosity for technology. My portfolio showcases my ability to turn ideas into reality through the use of HTML, CSS, JavaScript, NodeJs, ExpressJs, MongoDB React and other web deveolping technologies. I am constantly pushing my limits and striving to learn new skills.
                 </p>
             </div>
         </section>
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,7 +3,7 @@ const Main = () => {
     return (
         <main className='main-section'>
             <div className="main-text">
-                <h1 className="main-header">hello there! I'am aden</h1>
+                <h1 className="main-header">Hello there! I'am Adnan</h1>
                 <p> I am a full-stack developer.</p>
                 <a href={Pdf} className='profile-links' target='_blank' rel='noopener noreferrer'>Resume</a> 
                 <a href="https://github.com/Adnanyu" target='_blank' rel="noreferrer" className='profile-links'>Github</a>
