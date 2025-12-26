9940e84e2ef2adffb598ddd2cfe267d91d2d4819
Adnan Yusuf <adnan@Adnans-MacBook-Air.local>
2025-12-25 23:27:57 -0500

    added typing animation

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,12 +1,14 @@
 import Pdf from '../assets/Resume.pdf'
+import TypingLoop from './TypeLoop'
 const Main = () => {
+    
     return (
         <main className='main-section'>
             <div className="main-text">
-                <h1 className="main-header">Hello there! I'am Adnan</h1>
-                <p> I am a full-stack developer.</p>
+                <h1 className="main-header">Hello there! my name is <span style={{ color: 'rgba(255, 0, 0, 1)' }}>Adnan.</span></h1>
+                <TypingLoop />
                 <a href={Pdf} className='profile-links' target='_blank' rel='noopener noreferrer'>Resume</a> 
-                <a href="https://github.com/Adnanyu" target='_blank' rel="noreferrer" className='profile-links'>Github</a>
+                <a href="https://github.com/Adnanyu" target='_blank' rel="noreferrer" className='profile-links'>GitHub</a>
             </div>
         </main>
     )
